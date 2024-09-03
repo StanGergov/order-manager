f485630bf1470365783a2bbe24dbb65d058831c9
StanGergov <81183207+StanGergov@users.noreply.github.com>
2024-09-03 17:23:21 +0300

    fixed conflict

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,97 +1,47 @@
 import { Routes } from '@angular/router';
-<<<<<<< Updated upstream
 import { HomeComponent } from './home/home.component';
 import { ProductsComponent } from './products/products.component';
 import { ClientsComponent } from './clients/clients.component';
 import { OrdersComponent } from './orders/orders.component';
 import { NewOrderComponent } from './new-order/new-order.component';
-
-=======
-<<<<<<< Updated upstream
-
-export const routes: Routes = [];
-=======
-import { HomeComponent } from './home/home.component';
-import { ProductsComponent } from './products/products.component';
-import { ClientsComponent } from './clients/clients.component';
-import { OrdersComponent } from './orders/orders.component';
-import { NewOrderComponent } from './new-order/new-order.component';
-<<<<<<< Updated upstream
-=======
 import { LoginComponent } from './login/login.component';
 import { AuthGuard } from './auth-guard.guard';
->>>>>>> Stashed changes
-
->>>>>>> Stashed changes
-
 
 export const routes: Routes = [
     {
         path: '',
         component: HomeComponent,
-<<<<<<< Updated upstream
-        title: 'Home page'
-=======
         title: 'Home page',
         canActivate: [AuthGuard]
->>>>>>> Stashed changes
     },
     {
         path: 'products',
         component: ProductsComponent,
-<<<<<<< Updated upstream
-        title: 'Products page'
-=======
         title: 'Products page',
         canActivate: [AuthGuard]
->>>>>>> Stashed changes
     },
     {
         path: 'clients',
         component: ClientsComponent,
-<<<<<<< Updated upstream
-        title: 'Clients page'
-=======
+
         title: 'Clients page',
         canActivate: [AuthGuard]
->>>>>>> Stashed changes
     },
     {
         path: 'orders',
         component: OrdersComponent,
-<<<<<<< Updated upstream
-        title: 'Orders page'
-=======
         title: 'Orders page',
         canActivate: [AuthGuard]
->>>>>>> Stashed changes
     },
     {
         path: 'new-order',
         component: NewOrderComponent,
-<<<<<<< Updated upstream
-        title: 'New order page'
-<<<<<<< Updated upstream
-=======
-=======
-<<<<<<< Updated upstream
-        title: 'New order page'
-=======
         title: 'New order page',
         canActivate: [AuthGuard]
->>>>>>> Stashed changes
     },
     {
         path: 'login',
         component: LoginComponent,
         title: 'Login page'
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
     }
 ];
-=======
->>>>>>> Stashed changes
-    }
-];
->>>>>>> Stashed changes
->>>>>>> Stashed changes
--- src/app/auth-guard.guard.ts
+++ src/app/auth-guard.guard.ts
@@ -3,20 +3,19 @@ import { ActivatedRouteSnapshot, CanActivate, CanActivateFn, Router, RouterState
 import { AuthService } from './auth.service';
 import { map, Observable, of } from 'rxjs';
 
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 export class AuthGuard implements CanActivate {
 
-    constructor(private authService: AuthService, private router: Router) {}
+  constructor(private authService: AuthService, private router: Router) { }
 
-    canActivate(route:ActivatedRouteSnapshot, state:RouterStateSnapshot): Observable<boolean> {
-       return this.authService.user$.pipe(map((user: any) => {
-        console.log(user);
-        if (user) {
-          return true;
-        } else {
-          this.router.navigateByUrl('/login');
-          return false;
-        }
-       }));
-    }
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> {
+    return this.authService.user$.pipe(map((user: any) => {
+      if (user) {
+        return true;
+      } else {
+        this.router.navigateByUrl('/login');
+        return false;
+      }
+    }));
+  }
 }
