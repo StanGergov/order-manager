33af678e1c1984d32159bae196e3c3a390b4d3f0
StanGergov <81183207+StanGergov@users.noreply.github.com>
2024-09-03 17:26:25 +0300

    fixed conflict

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,19 +1,11 @@
 import { Component } from '@angular/core';
-<<<<<<< Updated upstream
-=======
 import { RouterOutlet, RouterLink } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
-
->>>>>>> Stashed changes
 
 @Component({
   selector: 'app-home',
   standalone: true,
-<<<<<<< Updated upstream
-  imports: [],
-=======
   imports: [RouterOutlet, RouterLink, MatButtonModule],
->>>>>>> Stashed changes
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
