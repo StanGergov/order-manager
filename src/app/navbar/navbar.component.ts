656f40b7710ac4ded72c1afcf4eebf535b67139a
StanGergov <81183207+StanGergov@users.noreply.github.com>
2024-09-03 11:21:26 +0300

    Style changes + added logout button

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet, RouterLink } from '@angular/router';
+import { AuthService } from '../auth.service';
+import { MatButtonModule } from '@angular/material/button';
+
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [ RouterOutlet, RouterLink ],
+  imports: [ RouterOutlet, RouterLink, MatButtonModule ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
+  constructor(private authService: AuthService){}
 
+  public logout(): void {
+    this.authService.logout();
+  }
 }
