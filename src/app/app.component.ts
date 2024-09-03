0ed639f0fdc96a39e6e1eabd5dd7380ac86aa742
StanGergov <81183207+StanGergov@users.noreply.github.com>
2024-09-03 11:31:36 +0300

    Created auth service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,33 @@
 import { Component } from '@angular/core';
 import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
+import { AuthService } from './auth.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
+<<<<<<< Updated upstream
   imports: [RouterOutlet, RouterLink, NavbarComponent],
+=======
+<<<<<<< Updated upstream
+  imports: [RouterOutlet, NavbarComponent],
+=======
+  imports: [
+    RouterOutlet,
+    RouterLink,
+    NavbarComponent,
+    CommonModule 
+  ],
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'order-manager';
+
+  constructor(public authService: AuthService) {
+
+  }
 }
