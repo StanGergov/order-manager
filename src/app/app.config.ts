747a7d81d7021c9d8fa43d98b0ec1280b99a4b6e
StanGergov <81183207+StanGergov@users.noreply.github.com>
2024-09-03 11:18:41 +0300

    Firebase configuration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -7,8 +7,26 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
-import { getDatabase, provideDatabase } from '@angular/fire/database';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA3BMvcqTnv-gaX7jW6qrPkfoz1t1rt7AQ",
+  authDomain: "order-manager-3d957.firebaseapp.com",
+  databaseURL: "https://order-manager-3d957-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "order-manager-3d957",
+  storageBucket: "order-manager-3d957.appspot.com",
+  messagingSenderId: "276529807823",
+  appId: "1:276529807823:web:bbf8ddef36f6b6d1a792ba",
+};
+
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideStore(), provideAnimationsAsync(), provideFirebaseApp(() => initializeApp({"projectId":"order-manager-3d957","appId":"1:276529807823:web:bbf8ddef36f6b6d1a792ba","databaseURL":"https://order-manager-3d957-default-rtdb.europe-west1.firebasedatabase.app","storageBucket":"order-manager-3d957.appspot.com","apiKey":"AIzaSyA3BMvcqTnv-gaX7jW6qrPkfoz1t1rt7AQ","authDomain":"order-manager-3d957.firebaseapp.com","messagingSenderId":"276529807823"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideDatabase(() => getDatabase())]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideStore(),
+    provideAnimationsAsync(),
+    provideFirebaseApp(() => initializeApp(firebaseConfig)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()), 
+  ]
 };
