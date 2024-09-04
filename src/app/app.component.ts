5b75f6a96a313488a402ef6447bc9fef18830971
StanGergov <81183207+StanGergov@users.noreply.github.com>
2024-09-04 12:06:06 +0300

    conflict fixed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,20 +7,12 @@ import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-root',
   standalone: true,
-<<<<<<< Updated upstream
-  imports: [RouterOutlet, RouterLink, NavbarComponent],
-=======
-<<<<<<< Updated upstream
-  imports: [RouterOutlet, NavbarComponent],
-=======
   imports: [
     RouterOutlet,
     RouterLink,
     NavbarComponent,
     CommonModule 
   ],
->>>>>>> Stashed changes
->>>>>>> Stashed changes
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
